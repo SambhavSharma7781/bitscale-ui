60b8659594c2015639cf2fad90838e299794f28d
SambhavSharma7781 <sambhav7717@gmail.com>
2026-06-12 17:05:25 +0530

    fix: set default theme to light and remove system theme support in ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           <LayoutShell>{children}</LayoutShell>
         </ThemeProvider>
       </body>
